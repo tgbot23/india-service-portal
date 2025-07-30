5a926315efb7b2eb1f4157c4b6144329f9843001
tgbot23 <tgbotvikash@gmail.com>
2025-07-30 17:35:26 +0530

    Create About.js

---
+++ frontend/src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>About Us</h1>
+      <p>This website provides important links and services for Indian users.</p>
+    </div>
+  );
+}
+
+export default About;
