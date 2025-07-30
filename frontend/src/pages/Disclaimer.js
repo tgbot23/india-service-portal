914bab0567cda8d8da0b00a6922172c35f73c47f
tgbot23 <tgbotvikash@gmail.com>
2025-07-30 17:39:10 +0530

    Create Disclaimer.js

---
+++ frontend/src/pages/Disclaimer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Disclaimer() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Disclaimer</h1>
+      <p>This website is not affiliated with any government entity. It is for informational purposes only.</p>
+    </div>
+  );
+}
+
+export default Disclaimer;
