0c943eaa4a14ddd0d47e1335d437313f4388c3b0
tgbot23 <tgbotvikash@gmail.com>
2025-07-30 17:37:36 +0530

    Create Contact.js

---
+++ frontend/src/pages/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Contact Us</h1>
+      <p>Email: vikashkr2611@live.com</p>
+    </div>
+  );
+}
+
+export default Contact;
