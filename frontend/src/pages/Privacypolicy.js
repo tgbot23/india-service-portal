9a41e9ec56b648bf51a24e72c79152d026cfbd41
tgbot23 <tgbotvikash@gmail.com>
2025-07-30 17:38:24 +0530

    Create Privacypolicy.js

---
+++ frontend/src/pages/Privacypolicy.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function PrivacyPolicy() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Privacy Policy</h1>
+      <p>We respect your privacy. No personal data is stored on this website.</p>
+    </div>
+  );
+}
+
+export default PrivacyPolicy;
