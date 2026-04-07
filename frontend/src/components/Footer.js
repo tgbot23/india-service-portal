e6adbd65aa85d21ce5d29cfd083b626c5c38ebb2
tgbot23 <tgbotvikash@gmail.com>
2026-04-07 14:33:48 +0530

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,1 +1,13 @@
-Powered by Mithila Computer Academy
+import React from "react";
+
+function Footer() {
+  return (
+    <div style={{textAlign:"center", padding:"10px", marginTop:"20px"}}>
+      <p>© 2026 OnlineApplyHelp.in</p>
+      <p>Managed by Mithila Computer Academy</p>
+      <p>MSME Registered (Udyam No: UDYAM-BR-20-0059295)</p>
+    </div>
+  );
+}
+
+export default Footer;
